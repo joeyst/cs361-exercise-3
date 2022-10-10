005470d89aeaa0156bb624c6b516ba14b96eed4f
joeyst <joeystenbeck@gmail.com>
2022-10-10 12:55:59 -0700

    Add schedule to initialize method and replace passing it as an argument with call to get method.

--- main.rb
+++ main.rb
@@ -3,11 +3,15 @@ class Student
   attr_accessor :schedule
   MAX_COURSES = 5
 
-  def remove_from_schedule(course, schedule, quarter_id)
+  def initialize(schedule)
+    @schedule = schedule
+  end
+
+  def remove_from_schedule(course, quarter_id)
     schedule.quarter[quarter_id].course_list.remove(course)
   end
 
-  def add_to_schedule(course, schedule, quarter_id)
+  def add_to_schedule(course, quarter_id)
     quarter     = schedule.quarter[quarter_id]
     course_list = quarter.course_list
     max_courses = course_list.maximum_number_of_courses
